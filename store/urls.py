918cd03663112f4743ca82e7b36e8a62d6e117b2
Leoper <dleonardo1221@gmail.com>
2025-08-30 11:41:17 -0500

    Pruebas y correcciones en los enlaces

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'store'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.product_list, name='products'),
